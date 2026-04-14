205a3057669087c075ab3bf138ea9c7609bc7888
Emin Samadzade <eminsemedzade7@gmail.com>
2026-04-14 12:29:22 +0400

    feat: Add domain models (User, Product, Category, Order, OrderItem)

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+namespace ECommerceApp.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    public decimal TotalAmount { get; set; }
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
